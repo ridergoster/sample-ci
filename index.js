bc1ffb75878e58ea1470d7f8f1cb1426f025a1b3
Vincentk <vincent.kocupyr@gmail.com>
2016-06-03 15:00:34 +0200

    ftr(contact): add GET /contacts

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', function(req, res) {
   res.send('Hello world');
 });
 
+app.get('/contacts', function(req, res, next) {
+  res.send([]);
+});
+
 console.log('listening on port 8080');
 app.listen(8080);
 
